f6b9a3dddaea4038933619dfcdf02ee9bced227b
s-marsi <soufianemarsi1999@gmail.com>
2024-11-07 19:25:47 +0100

    modify libft

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -1,23 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: smarsi <smarsi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 13:13:39 by smarsi            #+#    #+#             */
-/*   Updated: 2024/11/06 21:09:45 by smarsi           ###   ########.fr       */
+/*   Updated: 2024/11/07 18:13:09 by smarsi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strcmp(const char *s1, const char *s2)
+int	ft_strncmp(const char *s1, const char *s2, size_t len)
 {
-	while ((*s1 && *s2) && *s1 == *s2)
+	while ((*s1 && *s2) && *s1 == *s2 && len-- > 1)
 	{
 		s1++;
-		s2++;/*  */
+		s2++;
 	}
 	return ((unsigned char)*s1 - (unsigned char)*s2);
 }
